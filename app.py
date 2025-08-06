a825eb7392aa7efbfc73105056f887763664593f
uma9801 <uma_w0@hotmail.com>
2025-08-06 23:57:41 +0900

    6th commit

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 import os
 import streamlit as st
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
 from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 
